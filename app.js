3f296c931b32ac6d2b672810c0005ed74fa51ccf
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2023-12-08 23:05:51 -0300

    Uso do GraphQL

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ import cors from 'cors'
 import winston from 'winston'
 import { promises as fs } from 'fs'
 import AccountRouter from './routes/account.router.js'
+import { buildSchema } from 'graphql'
+import { graphqlHTTP } from 'express-graphql'
+import AccountService from './services/account.service.js'
+import Schema from './schema/index.js'
+
 
 const { readFile, writeFile } = fs
 
@@ -27,10 +32,57 @@ global.logger = winston.createLogger({
     )
 })
 
+// const schema = buildSchema(`
+//     type Account {
+//         id: Int
+//         name: String
+//         balance: Float
+//     }
+
+//     input AccountInput {
+//         id: Int
+//         name: String
+//         balance: Float
+//     }
+
+//     type Query {
+//         getAccounts: [Account]
+//         getAccount(id: Int): Account
+//     }
+
+//     type Mutation {
+//         createAccount(account: AccountInput): Account
+//         deleteAccount(id: Int): Boolean
+//         updateAccount(account: AccountInput): Account
+//     }
+// `)
+
+// const root = {
+//     getAccounts: () => AccountService.getAccounts(),
+//     getAccount(args) {
+//         return AccountService.getAccountById(args.id)
+//     },
+//     createAccount({account}) {
+//         return AccountService.createAccount(account)
+//     },
+//     deleteAccount(args) {
+//         AccountService.deleteAccount(args.id)
+//     },
+//     updateAccount({account}) {
+//         return AccountService.updateAccount(account)
+//     }
+// }
+
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use("/account", AccountRouter)
+app.use("/graphql", graphqlHTTP({
+    schema: Schema,
+    // rootValue: root,
+    graphiql: true
+}))
+
 const port = 3000
 
 app.listen(port, async () => {
